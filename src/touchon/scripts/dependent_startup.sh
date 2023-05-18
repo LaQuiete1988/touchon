ef03bb4f2462c73bdff1ff761dca0b6c6acc4fff
Vitaliy Nikolaev <vn@touchon.tech>
2023-05-18 12:05:43 +0300

    добавлен скрипт управления кондиционерами

--- src/touchon/scripts/dependent_startup.sh
+++ src/touchon/scripts/dependent_startup.sh
@@ -11,9 +11,9 @@ sleep 5
 source /opt/touchon/scripts/mysql_setup.sh
 
 timeZone=$(mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -sse "SELECT value FROM $MYSQL_DATABASE.settings WHERE name='time_zone';")
-ln -snf /usr/share/zoneinfo/$timeZone /etc/localtime && echo $timeZone > /etc/timezone
-sed -i 's,.*date.timezone =.*,date.timezone = '"$timeZone"',g' /etc/php/7.4/fpm/php.ini
-sed -i 's,.*date.timezone =.*,date.timezone = '"$timeZone"',g' /etc/php/7.4/cli/php.ini
+ln -snf /usr/share/zoneinfo/${timeZone:-Europe/Moscow} /etc/localtime && echo ${timeZone:-Europe/Moscow} > /etc/timezone
+sed -i 's,.*date.timezone =.*,date.timezone = '"${timeZone:-Europe/Moscow}"',g' /etc/php/7.4/fpm/php.ini
+sed -i 's,.*date.timezone =.*,date.timezone = '"${timeZone:-Europe/Moscow}"',g' /etc/php/7.4/cli/php.ini
 
 supervisorctl start php-fpm
 
@@ -24,4 +24,4 @@ supervisorctl start nginx
 source /opt/touchon/scripts/core_installation.sh
 source /opt/touchon/scripts/adm_installation.sh
 
-sed -i 's,APP_TIMEZONE=.*,APP_TIMEZONE='"$timeZone"',g' ${WORK_DIR}/adm/.env
+sed -i 's,APP_TIMEZONE=.*,APP_TIMEZONE='"${timeZone:-Europe/Moscow}"',g' ${WORK_DIR}/adm/.env
