a99bafc03c15dfdf398ca95e1bead94060d9cb93
Vitaliy Nikolaev <vn@touchon.tech>
2023-10-03 15:51:32 +0300

    Добавлена возможность собрать окружение с php8.1

--- src/scripts/dependent_startup.sh
+++ src/scripts/dependent_startup.sh
@@ -13,8 +13,9 @@ mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u$MYSQL_USER -p$MYSQL_ROOT_PASS
 
 timeZone=$(mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -sse "SELECT value FROM $MYSQL_DATABASE.settings WHERE name='time_zone';")
 ln -snf /usr/share/zoneinfo/${timeZone:-Europe/Moscow} /etc/localtime && echo ${timeZone:-Europe/Moscow} > /etc/timezone
-sed -i 's,.*date.timezone =.*,date.timezone = '"${timeZone:-Europe/Moscow}"',g' /etc/php/7.4/fpm/php.ini
-sed -i 's,.*date.timezone =.*,date.timezone = '"${timeZone:-Europe/Moscow}"',g' /etc/php/7.4/cli/php.ini
+phpVersion=$(php -v | head -n 1 | awk '/PHP/ {print $2}' | cut -d. -f1,2)
+sed -i 's,.*date.timezone =.*,date.timezone = '"${timeZone:-Europe/Moscow}"',g' /etc/php/${phpVersion}/fpm/php.ini
+sed -i 's,.*date.timezone =.*,date.timezone = '"${timeZone:-Europe/Moscow}"',g' /etc/php/${phpVersion}/cli/php.ini
 mysql -u$MYSQL_USER -p$MYSQL_ROOT_PASSWORD mysql -sse "SET GLOBAL time_zone = '$timeZone';"
 
 supervisorctl start php-fpm
@@ -23,7 +24,6 @@ envsubst "\$WORK_DIR" < /opt/touchon/configs/nginx.conf.template > /opt/touchon/
 
 supervisorctl start nginx
 
-# source /opt/touchon/scripts/adm_installation.sh
-# source /opt/touchon/scripts/core_installation.sh
-
 sed -i 's,APP_TIMEZONE=.*,APP_TIMEZONE='"${timeZone:-Europe/Moscow}"',g' ${WORK_DIR}/adm/.env
+
+chmod +x /opt/touchon/scripts/* && chmod +x /opt/touchon/scripts/rs_control/rs_control \
