8733c0ea019f7ec9525c201ca1bd33e150eb3e53
Vitaliy Nikolaev <vn@touchon.tech>
2024-06-25 12:10:28 +0300

    minor changes

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 docker-compose up -d --build
-docker-compose exec -ti touchon /opt/touchon/scripts/dependent_startup.sh
+# docker-compose exec -ti touchon /opt/touchon/scripts/dependent_startup.sh
 containerId=$(docker ps -aqf "name=touchon")
-docker commit $containerId touchon:1.0-jethome
-docker tag touchon:1.0-jethome 178.57.106.190:5000/touchon:1.0-jethome
-docker push 178.57.106.190:5000/touchon:1.0-jethome
+docker commit $containerId touchon:1.1-jethome
+docker tag touchon:1.1-jethome 178.57.106.190:5000/touchon:1.1-jethome
+docker push 178.57.106.190:5000/touchon:1.1-jethome
