ef7fc8b75891f97a455445e48c21514d95f28365
Vitaliy Nikolaev <vn@touchon.tech>
2024-06-25 14:30:00 +0300

    minor changes

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 docker-compose up -d --build
-# docker-compose exec -ti touchon /opt/touchon/scripts/dependent_startup.sh
+docker exec touchon bash -c 'php ${WORK_DIR}/adm/artisan migrate --seed --force'
+docker exec touchon bash -c 'php ${WORK_DIR}/adm/artisan create:user superadmin ${ADM_SUPERADMIN_USER} ${ADM_SUPERADMIN_PASSWORD}'
+docker exec touchon bash -c 'php ${WORK_DIR}/adm/artisan create:user superadmin ${ADM_PARTNER_USER} ${ADM_PARTNER_PASSWORD}'
 containerId=$(docker ps -aqf "name=touchon")
 docker commit $containerId touchon:1.1-jethome
 docker tag touchon:1.1-jethome 178.57.106.190:5000/touchon:1.1-jethome
